4317b07c1e949eba3367fdeacfc66c6d62b2f3fe
shinya.sato <shinya.sato@gmo-media.jp>
2016-08-17 17:05:38 +0900

    RunIdIncrementer

--- src/main/java/batch/BatchConfiguration.java
+++ src/main/java/batch/BatchConfiguration.java
@@ -1,11 +1,16 @@
 package batch;
 
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -40,6 +45,7 @@ public class BatchConfiguration {
     @Bean
     public Job job(Step step1) throws Exception {
         return jobBuilderFactory.get("job1")
+                .incrementer(new RunIdIncrementer())
                 .start(step1)
                 .build();
     }
